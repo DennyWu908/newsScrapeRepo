b72117fc02bd9fc2874c59f349ddac3b0f6e2291
Denny Wu <denny.wu87@gmail.com>
2018-01-31 16:12:47 -0800

    Added code to scrape news article titles and text

---
+++ models/Article.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+
+  headline: {
+    type: String,
+    required: true
+  },
+
+  summary: {
+    type: String,
+    required: true
+  },
+
+  url: {
+    type: String,
+    required: true
+  },
+
+  saved: {
+    type: Boolean,
+    required: true
+  },
+
+  note: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
+
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
